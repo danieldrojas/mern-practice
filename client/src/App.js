aa6a9ae787d9fc0523c9d73e0096533eb38690ec
Daniel Rojas <daniel.drojas@outlook.com>
2020-08-04 21:21:17 -0400

    added proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios'
 
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("/api/config")
+      .then((res) => {
+        console.log(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+  }, []);
   return (
     <div>
       <h1>Hello Wold</h1>
--- server.js
+++ server.js
@@ -6,22 +6,26 @@ var app = express();
 const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true, useNewUrlParser: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/api/config", (req, res) => {
     res.json({
-        success: true,
-        message: "Successfully connected"
+        success: true
     });
 })
 
 mongoose
-    .connect(process.env.MONGOD_URI || "mongodb://localhost/mern-practice")
+    .connect(process.env.MONGOD_URI || "mongodb://localhost/mern-practice",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
     .then(() => {
         console.log("connected to the database")
     })
     .catch((err) => {
+        console.log("unable to connect to the database")
         console.log(err)
     })
 
